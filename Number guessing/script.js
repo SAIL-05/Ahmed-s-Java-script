6897bfa2a8d25d283e8902fd8dd923c6bd4ff873
untitled <ahmedsuleiman940@gmail.com>
2025-05-12 10:43:19 +0100

    loop guessing game

--- Number guessing/script.js
+++ Number guessing/script.js
@@ -2,9 +2,12 @@
 
 // Write a simple number guessing game. Provide a secret number and a guess. Based on those numbers give players clues if their guess is higher, lower or correct.
 
+
+
 let answer = 50
 let guess = Number(prompt(`Enter your lucky number`))
 
+while (guess !== answer){
 if (guess < answer) {
     alert('Not lucky, try something HIGHER')
 }
@@ -14,3 +17,6 @@ else if (guess > answer) {
 else {
     alert(`you are lucky, the number is ${answer}`)
 }
+    guess = Number(prompt(`Enter your lucky number`))
+}
+alert(`you are lucky, the number is ${answer}`)
