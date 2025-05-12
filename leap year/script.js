e459fcb714ec2b2b432707c85f84c0fb720eea30
untitled <ahmedsuleiman940@gmail.com>
2025-05-12 09:55:35 +0100

    leap year

---
+++ leap year/script.js
@@ -0,0 +1,18 @@
+
+
+// Write a program that determines if a year is a leap year.
+
+let daysOfTheYear = Number(prompt('Input the days in the year'))
+
+
+if (daysOfTheYear === 365){
+    alert(`It is Not a LEAP YEAR 😑😑😑`)
+}
+else if (daysOfTheYear === 366){
+    alert(`It is a LEAP YEAR 😁😁😁😊`)
+}
+else {
+    alert(`it is not a valid year number`)
+}
+
+
